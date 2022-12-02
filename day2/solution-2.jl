c50e8466737f6801127f7be20676561de21d361b
Cassandra McCormack <cassandra.m.mccormack@gmail.com>
2022-12-02 12:50:21 +0000

    solution 2, absolutely rubbish

--- day2/solution-2.jl
+++ day2/solution-2.jl
@@ -7,3 +7,58 @@ f = open("input.txt")
 
 @enum OpponentPlay A B C
 @enum MyPlay X Y Z
+
+function asEnum(play)
+    if cmp(play,'A') == 0
+        return Int(A)
+    elseif cmp(play,'B') == 0
+        return Int(B)
+    elseif cmp(play,'C') == 0
+        return Int(C)
+    elseif cmp(play,'X') == 0
+        return Int(X)
+    elseif cmp(play,'Y') == 0
+        return Int(Y)
+    elseif cmp(play,'Z') == 0
+        return Int(Z)
+    end
+end
+
+score = 0
+for line in eachline(f)
+    println("round ", line)
+    opponent = line[1]
+    me = line[3]
+    play = 0
+    roundResult = 0
+    # must draw, choose same as opponent
+    if cmp(me, 'Y') == 0
+        roundResult = 3
+        play = opponent
+    # must lose
+    elseif cmp(me, 'X') == 0
+        roundResult = 0
+        if cmp(opponent,'A') == 0
+            play = 'C'
+        elseif cmp(opponent, 'B') == 0
+            play = 'A'
+        elseif cmp(opponent, 'C') == 0
+            play = 'B'
+        end
+        println(roundResult + asEnum(play) + 1)
+    # must win
+    elseif cmp(me, 'Z') == 0
+        roundResult = 6
+        if cmp(opponent,'A') == 0
+            play = 'B'
+        elseif cmp(opponent, 'B') == 0
+            play = 'C'
+        elseif cmp(opponent, 'C') == 0
+            play = 'A'
+        end
+        println(roundResult + asEnum(play) + 1)
+    end
+    global score = score + asEnum(play) + 1 + roundResult
+end
+
+println(score)
